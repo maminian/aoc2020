0bd4ff0d9ee08410297bd0b7e7375f945e6f12b9
maminian <manuchehr.aminian@gmail.com>
2020-12-10 01:29:33 -0800

    cleaner day10

--- dec10/solution.jl
+++ dec10/solution.jl
@@ -1,63 +1,35 @@
 push!(LOAD_PATH, abspath("../"))
 import loaders
 
-# constrained sequences...
+# combinatorics of subsequences...
 
 input = loaders.listloader("input", Int128)
-input = vcat(Int128(0),input)
-#input = vcat(Int128(0),input,Int128(input[end]+3))
+input = vcat(Int128(0),input,Int128(maximum(input)+3))  # include implied head and tail
 
 o = sortperm(input)
 
-dd = diff( input[o] )
-#dd = vcat(input[o[1]], dd)
+diffs = diff( input[o] )
 
-onec = sum(dd .== 1)
-threec = (1 + sum(dd .== 3))
+onec = sum(diffs .== 1)
+threec = sum(diffs .== 3)
 
+println("\nPart 1\n================")
+println("First two numbers are the counts; third is the product.")
 println([onec, threec, onec*threec])
 
 ####################
 #
 # part 2
 
-### Just leaving this here for github historians to appreciate
-# how bad at recursion I am.
-#
-
-#global mem = Dict()
-#mem[()] = 0
-#mem[(1,)] = 1
-#mem[(1,1)] = 2
-#mem[(2,1)] = 2
-#mem[(1,2)] = 2
-#mem[(2,0)] = 
-#mem[(0,2)] = 
-
-
-#function countperm(seq)
-#    global mem
-#    if seq in keys(mem)
-#        return mem[seq]
-#    else
-#        global allprod = 0
-#        for i=1:length(seq)-1
-#            left = seq[1:i]
-#            right = seq[i+1:end]
-#            global allprod += countperm(left) * countperm(right)
-#        end
-#    end
-#    global mem[seq] = allprod
-#    return allprod
-#end
-
 # Explicit formula for valid sequences of diff 1 ending 
-# just before a jump size three, of length i. for example, 
-# c[1]=1 represents subsequences of (1,4), 
-# c[2]=2 represents subsequences of (1,2,5), 
-# c[3]=4 represents subsequences of (1,2,3,6)
+# just before a jump size three, of length i. 
+# 
+# For example, 
+# c[1]=1 represents number of subsequences of (1,4), 
+# c[2]=2 represents number of subsequences of (1,2,5), 
+# c[3]=4 represents number of subsequences of (1,2,3,6)
 
-global cseq = zeros(Int128, length(input))
+global cseq = zeros(Int128, onec)   # upper bound on consecutive ones
 cseq[1:4] = [1,2,4,7]
 for i=5:length(cseq)
     global cseq[i] = 2*cseq[i-1] - cseq[i-4]
@@ -65,14 +37,6 @@ end
 
 global l=1
 factors = []
-
-# need to tack on ending to make loop work cleaner.
-input2 = vcat(input,Int128(maximum(input)+3))
-o2 = sortperm(input2)
-
-diffs = diff(input2[o2])
-global l=1
-
 for (r,v) in enumerate( diffs )
     if (v==3)
         consec = r-l
@@ -83,5 +47,5 @@ for (r,v) in enumerate( diffs )
     end
 end
 
-println("Part 2\n================")
-println(prod(factors))
+println("\nPart 2\n================")
+println("Number of combinations:\n", prod(factors))
